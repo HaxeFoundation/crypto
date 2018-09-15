80b2ee905f8ce84620c1bc221fb04984fc6bf14d
flashultra <m_mail01@abv.bg>
2018-09-15 16:03:33 +0300

    Correct str2blks . Add unicode test sha224, sha256

--- tests/src/unit/crypto/Sha224Test.hx
+++ tests/src/unit/crypto/Sha224Test.hx
@@ -7,16 +7,13 @@ import haxe.crypto.Sha224;
 class Sha224Test extends Test
 {
 
-    public function test_encrypt():Void
+    public function test_hash_sha224():Void
     {
-       eq(haxe.crypto.Sha224.encode(""), "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f");
-	   eq(haxe.crypto.Sha224.encode("abc"), "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7");
-    }
-
-    public function test_decrypt():Void
-    {
-      eq(haxe.crypto.Sha224.encode("The quick brown fox jumps over the lazy dog"), "730e109bd7a8a32b1cb9d9a09aa2325d2430587ddbc0c38bad911525");
-	  eq(haxe.crypto.Sha224.encode("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"), "75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525");
-	  eq(haxe.crypto.Sha224.encode("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"), "c97ca9a559850ce97a04a96def6d99a9e0e0e2ab14e6b8df265fc0b3");
+       eq(Sha224.encode(""), "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f");
+	   eq(Sha224.encode("abc"), "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7");
+	   eq(Sha224.encode("The quick brown fox jumps over the lazy dog"), "730e109bd7a8a32b1cb9d9a09aa2325d2430587ddbc0c38bad911525");
+	   eq(Sha224.encode("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"), "75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525");
+	   eq(Sha224.encode("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"), "c97ca9a559850ce97a04a96def6d99a9e0e0e2ab14e6b8df265fc0b3");
+  	   eq(Sha224.encode("éあ😂") == "d7967c5f27bd6868e276647583c55ab09d5f45b40610a3d9c6d91b90");
     }
 }
--- tests/src/unit/crypto/Sha256Test.hx
+++ tests/src/unit/crypto/Sha256Test.hx
@@ -7,13 +7,10 @@ import haxe.crypto.Sha256;
 class Sha256Test extends Test
 {
 
-    public function test_encrypt():Void
+    public function test_hash_sha256():Void
     {
-       eq(haxe.crypto.Sha256.encode(""), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
-    }
-
-    public function test_decrypt():Void
-    {
-      eq(haxe.crypto.Sha256.encode("The quick brown fox jumps over the lazy dog"), "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592");
+       eq(Sha256.encode(""), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+	   eq(Sha256.encode("The quick brown fox jumps over the lazy dog"), "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592");
+	   eq(Sha256.encode("éあ😂") == "d0230b8d8ac2d6d0dbcee11ad0e0eaa68a6565347261871dc241571cab591676");
     }
 }
