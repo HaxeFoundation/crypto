0141ee712a69f92e0b3239483c3af77539a93b42
flashultra <m_mail01@abv.bg>
2022-10-03 10:34:46 +0300

    Formatting code

--- src/haxe/crypto/mode/CBC.hx
+++ src/haxe/crypto/mode/CBC.hx
@@ -6,21 +6,21 @@ class CBC
 {
     public static function encrypt( src : Bytes, iv : Bytes, blockSize : Int, encryptBlock : Bytes->Int->Bytes->Int->Void) : Void
     {
-		var vector = iv.getData();
-		var i : Int = 0;
-		var len : Int = src.length;
-		var ivpos : Int = 0;
-		while (i < len)
-		{
-			for (j in 0...blockSize)
-			{
-				src.set(i + j, src.get(i + j) ^ Bytes.fastGet(vector, ivpos + j) );
-			}
-			encryptBlock(src, i, src , i);
-			ivpos = i;
-			vector = src.getData();
-			i += blockSize;
-		}
+        var vector = iv.getData();
+        var i : Int = 0;
+        var len : Int = src.length;
+        var ivpos : Int = 0;
+        while (i < len)
+        {
+            for (j in 0...blockSize)
+            {
+                src.set(i + j, src.get(i + j) ^ Bytes.fastGet(vector, ivpos + j) );
+            }
+            encryptBlock(src, i, src , i);
+            ivpos = i;
+            vector = src.getData();
+            i += blockSize;
+        }
     }
 
     public static function decrypt( src : Bytes, iv : Bytes, blockSize : Int, decryptBlock : Bytes->Int->Bytes->Int->Void) : Void
@@ -31,15 +31,14 @@ class CBC
         {
             i -= blockSize;
             vpos -= blockSize;
-			
+            
             decryptBlock(src, i, src, i);
-
             if ( vpos < 0 ) {
                 for (j in 0 ... blockSize) 
-			    	src.set(j, src.get(j) ^ iv.get(j));
+                    src.set(j, src.get(j) ^ iv.get(j));
             } else {
                 for (j in 0 ... blockSize) 
-			    	src.set(i + j, src.get(i + j) ^ src.get(vpos + j));
+                    src.set(i + j, src.get(i + j) ^ src.get(vpos + j));
             }
         }
     }
--- src/haxe/crypto/mode/CFB.hx
+++ src/haxe/crypto/mode/CFB.hx
@@ -6,21 +6,20 @@ class CFB
 {
     public static function encrypt( src : Bytes, iv : Bytes, blockSize : Int, encryptBlock : Bytes->Int->Bytes->Int->Void) : Void
     {
-		var vector = iv.sub(0,iv.length);
-		var i : Int = 0;
-		var len : Int = src.length;
-		while (i < len)
-		{
+        var vector = iv.sub(0,iv.length);
+        var i : Int = 0;
+        var len : Int = src.length;
+        while (i < len)
+        {
             encryptBlock(vector, 0, vector , 0);
             var block:Int = (i+blockSize)>len?len-i:blockSize;
-			for (j in 0...block)
-			{
-			src.set(i + j, src.get(i + j) ^ vector.get(j) );
-			}
-
-			vector = src.sub(i,block);
-			i += blockSize;
-		}
+            for (j in 0...block)
+            {
+            src.set(i + j, src.get(i + j) ^ vector.get(j) );
+            }
+            vector = src.sub(i,block);
+            i += blockSize;
+        }
     }
 
     public static function decrypt( src : Bytes, iv : Bytes, blockSize : Int, decryptBlock : Bytes->Int->Bytes->Int->Void) : Void
@@ -29,17 +28,17 @@ class CFB
         var i : Int = 0;
         var len : Int = src.length;
         var chiperText : Bytes;
-		while (i < len)
-		{
+        while (i < len)
+        {
             decryptBlock(vector, 0, vector , 0);
-            var block:Int = (i+blockSize)>len?len-i:blockSize;	
+            var block:Int = (i+blockSize)>len?len-i:blockSize;
             chiperText = src.sub(i,block);
             for (j in 0...block)
-			{
-				src.set(i + j, src.get(i + j) ^ vector.get(j) );
-			}
+            {
+                src.set(i + j, src.get(i + j) ^ vector.get(j) );
+            }
             vector = chiperText.sub(0,chiperText.length);
-			i += blockSize;
-		}
+            i += blockSize;
+        }
     }
 }
