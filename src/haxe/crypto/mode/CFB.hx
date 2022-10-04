493eca2151c4e8babfec1fd6b49b0aae4215fc24
flashultra <m_mail01@abv.bg>
2022-10-04 09:52:23 +0300

    Optimize CFB mode

--- src/haxe/crypto/mode/CFB.hx
+++ src/haxe/crypto/mode/CFB.hx
@@ -16,8 +16,8 @@ class CFB
             for (j in 0...block)
             {
                 src.set(i + j, src.get(i + j) ^ vector.get(j) );
+                vector.set(j,src.get(i+j));
             }
-            vector = src.sub(i,block);
             i += blockSize;
         }
     }
@@ -27,15 +27,14 @@ class CFB
         var vector = iv.sub(0,iv.length);
         var i : Int = 0;
         var len : Int = src.length;
-        var chiperText : Bytes;
-		var vectorResult = Bytes.alloc(iv.length);
+        var vectorResult = Bytes.alloc(iv.length);
         while (i < len)
         {
             decryptBlock(vector, 0, vectorResult , 0);
             var block:Int = (i+blockSize)>len?len-i:blockSize;
-            vector = src.sub(i,block);
             for (j in 0...block)
             {
+                vector.set(j, src.get(i + j));
                 src.set(i + j, src.get(i + j) ^ vectorResult.get(j) );
             }
             i += blockSize;
