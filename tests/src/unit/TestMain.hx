fe649f59347363d626df509e1766fd6731017c76
flashultra <m_mail01@abv.bg>
2025-09-06 17:05:46 +0300

    Remove Web.isModNeko

--- tests/src/unit/TestMain.hx
+++ tests/src/unit/TestMain.hx
@@ -28,17 +28,6 @@ function main() {
 
 	var verbose = #if (cpp || neko || php) Sys.args().indexOf("-v") >= 0 #else false #end;
 
-	#if cs // "Turkey Test" - Issue #996
-	cs.system.threading.Thread.CurrentThread.CurrentCulture = new cs.system.globalization.CultureInfo('tr-TR');
-	cs.Lib.applyCultureChanges();
-	#end
-	#if neko
-	if (neko.Web.isModNeko)
-		neko.Web.setHeader("Content-Type", "text/plain");
-	#elseif php
-	if (php.Web.isModNeko)
-		php.Web.setHeader("Content-Type", "text/plain");
-	#end
 	#if !macro
 	trace("Generated at: " + HelperMacros.getCompilationDate());
 	#end
